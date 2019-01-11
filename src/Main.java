e8b02aafb8eed852e4a6b1eec4cae4c1baf47872
Wang Chen <zhanghuilian-98022@163.com>
2019-01-11 12:27:03 +0800

    The sort section

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,24 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         String data;
-        int n;
-        double time1, time2, time3;
+        long n;
+        int dilute;
+        double time1, time2;
         File fileRead = new File(
-                "D:\\Documents\\Introduction to Computer System\\Gnuplot\\lcs_numa_perf.out");
-        File fileWrite = new File(".\\lcs.data");
+                "D:\\Documents\\Introduction to Computer System\\Gnuplot\\sort_numa_perf.out");
+        File fileWrite = new File(".\\sort.data");
         PrintWriter output = new PrintWriter(fileWrite);
         Scanner input = new Scanner(fileRead);
-        output.println("#n time(CO_(2-way)) time(PACO_data_flow) time(P_way_PACO)");
+        output.println("#n dilute time(PBBS) time(PACO)");
         while (input.hasNextLine()) {
-            input.nextLine();
             data = input.nextLine();
-            n = Integer.parseInt(data.split(",")[0].trim().split(" ")[2]);
+            n = Long.parseLong(data.split(",")[0].trim().split(" ")[1]);
+            dilute = Integer.parseInt(data.split(",")[1].trim().split(" ")[3]);
+            time1 = Double.parseDouble(input.nextLine().split(" ")[2]);
             input.nextLine();
+            time2 = Double.parseDouble(input.nextLine().split(" ")[2]);
             input.nextLine();
-            time1 = Double.parseDouble(input.nextLine().split(" ")[6]);
-            time2 = Double.parseDouble(input.nextLine().split(" ")[5]);
-            time3 = Double.parseDouble(input.nextLine().split(" ")[6]);
-            input.nextLine();
-            input.nextLine();
-            output.println(n + " " + time1 + " " + time2 + " " + time3);
+            output.println(n + " " + dilute + " " + time1 + " " + time2);
             output.flush();
         }
         output.close();
